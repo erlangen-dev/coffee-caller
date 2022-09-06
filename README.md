3f3704a9c06af87e607be3b7326fd2a252d615d8
maxlmo <max.moeller55@gmail.com>
2022-09-06 11:49:09 +0200

    Implement initial server with socket.io

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # coffee-caller
+
+## Backend setup
+
+* Install [node](https://nodejs.org/en/)
+* Run `npm install --global yarn` to install yarn
+* Run `yarn` to install all dependencies
+* Run `yarn serve` to start backend in watch mode
+* Run `yarn start` to start backend
+
+Happy coding :)
---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+    console.log("Got new connection", socket);
+});
+
+const port = process.env.PORT || 4200;
+httpServer.listen(port);
+console.log(`Server running on port: ${port}`);
