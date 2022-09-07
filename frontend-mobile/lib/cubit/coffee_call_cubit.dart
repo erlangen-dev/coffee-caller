c3e69d10a5c99502b286ab0d1a115369cdc504bd
Tilman Adler <tilman-git@t-animal.de>
2022-09-07 15:54:32 +0200

    Use cubit for state handling

---
+++ frontend-mobile/lib/cubit/coffee_call_cubit.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:coffee_caller/cubit/coffee_call_state.dart';
+import 'package:coffee_caller/socket_client.dart';
+
+class CoffeeCallCubit extends Cubit<CoffeeCallState> {
+  SocketClient socket;
+
+  late StreamSubscription connectionStatusSubscription;
+  late StreamSubscription coffeeMessageSubscription;
+
+  CoffeeCallCubit({
+    required this.socket,
+  }) : super(const CoffeeCallState());
+
+  void init()  {
+    socket.connectStatusStream.listen((status)  {
+      switch(status) {
+        case SocketClientConnectStatus.disconnected:
+          emit(state.copyWith(status: CoffeeCallStatus.disconnected));
+          break;
+        case SocketClientConnectStatus.connected:
+          emit(state.copyWith(status: CoffeeCallStatus.connected));
+          break;
+        case SocketClientConnectStatus.error:
+          emit(state.copyWith(status: CoffeeCallStatus.disconnected));
+          break;
+        case SocketClientConnectStatus.connecting:
+          // do nothing
+          break;
+      }
+    });
+
+    socket.coffeMessageStream.listen((message) {
+      emit(state.copyWith(messages: [...state.messages, message]));
+    });
+  }
+
+  void connect() {
+    socket.init();
+  }
+
+  void sendMessage() {
+    socket.sendMessage();
+  }
+}
---
+++ frontend-mobile/lib/cubit/coffee_call_state.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'coffee_call_state.freezed.dart';
+
+enum CoffeeCallStatus { initial, connected, disconnected }
+
+@freezed
+class CoffeeCallState with _$CoffeeCallState {
+  const factory CoffeeCallState([
+    @Default(CoffeeCallStatus.initial) CoffeeCallStatus status,
+    @Default([]) List<String> messages,
+  ]) = _CoffeeCallState;
+}
