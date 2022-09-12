5256f82467e3e2f947a4a4b20b963a0392699510
maxlmo <max.moeller55@gmail.com>
2022-09-12 21:39:15 +0200

    Add missing commas for indentation

--- frontend-mobile/lib/view/settings_screen.dart
+++ frontend-mobile/lib/view/settings_screen.dart
@@ -6,9 +6,12 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Settings'),
-        ),
-        body: const Center(child: Text('Hello Settings')));
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: const Center(
+        child: Text('Hello Settings'),
+      ),
+    );
   }
 }
