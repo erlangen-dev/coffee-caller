73e6d97d6707f9f6c85f2061ff39cd0a523488b6
Tilman Adler <tilman-git@t-animal.de>
2022-09-13 16:35:51 +0200

    Add input field for username and store it

---
+++ frontend-web/src/settings/persistence.ts
@@ -0,0 +1,23 @@
+const keys = {
+  username: 'username'
+} as const
+
+type ActualKeys = typeof keys[keyof typeof keys];
+
+export const [storeUsername, getUsername] = storeLoadFunctions<string>(keys.username, '');
+
+function storeLoadFunctions<T>(key: ActualKeys, fallback: T): [(value: T)=>void, () =>T] {
+  return [
+    (value: T) => store<T>(key, value),
+    () => load<T>(key) ?? fallback
+  ];
+}
+
+function store<T>(key: ActualKeys, value: T) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function load<T>(key: ActualKeys): T | null {
+  const loadedValue = localStorage.getItem(key);
+  return loadedValue == null? null : JSON.parse(loadedValue);
+}
--- frontend-web/src/settings/settings.tsx
+++ frontend-web/src/settings/settings.tsx
@@ -1,5 +1,30 @@
-import { Component, For } from 'solid-js';
+import { Component, createEffect, createSignal, For, Show } from 'solid-js';
+import * as storage from './persistence';
+
+import style from './settings.module.css';
 
 export const Settings: Component = () => {
-    return <span>Settings!</span>;
+  const [username, setUsername] = createSignal(storage.getUsername());
+  const [usernameStored, setUsernameStored] = createSignal(false);
+
+  let storageTimeout: number | undefined;
+  function handleInput(event: InputEvent) {
+    setUsernameStored(false);
+
+    clearTimeout(storageTimeout);
+    storageTimeout = window.setTimeout(
+      () => {
+        storage.storeUsername((event.target as HTMLInputElement).value);
+        setUsername(storage.getUsername());
+        setUsernameStored(true);
+      },
+      800
+    );
+  }
+
+  return <label>
+    Username:
+    <input type="text" name="username" value={username()} onInput={handleInput} />
+    <Show when={usernameStored()}><span class={style.confirm}>✓</span></Show> 
+  </label>;
 }
