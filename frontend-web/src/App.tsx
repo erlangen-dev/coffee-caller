642af7a57d4d4f8e129dc780deb8592ee261e8e5
Tilman Adler <tilman-git@t-animal.de>
2022-09-13 12:43:21 +0200

    Move coffee call main component to subfolder

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,27 +1,15 @@
 import { Component, For } from 'solid-js';
 
 import styles from './App.module.css';
-import { SocketClient } from './socket-client';
-import { Protocol } from './protocol';
+
+import CoffeeCall from './coffee-call/coffee-call';
 
 const App: Component = () => {
-  const client = new SocketClient();
-  const protocol = new Protocol(client);
-
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <span class={styles.logo}>☕</span>
-        <div class={styles.buttonRow}>
-          <button onClick={() => protocol.join('Spiderpig')}>Join</button>
-          <button onClick={() => protocol.leave('Spiderpig')}>Leave</button>
-          <button onClick={() => protocol.start('Spiderpig')}>Start</button>
-        </div>
-        <ul>
-          <For each={protocol.messages()}>{(message) =>
-            <li>{message.name} {message.type}s a coffee call</li>
-          }</For>
-        </ul>
+        <CoffeeCall></CoffeeCall>
       </header>
     </div>
   );
