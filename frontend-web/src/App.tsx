490057ba4bbc3c1c415b2b696f57e34a5061d5e0
Tilman Adler <tilman-git@t-animal.de>
2022-09-13 12:53:56 +0200

    Add routing to separate config screen

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,16 +1,27 @@
+import { NavLink, Route, Routes } from '@solidjs/router';
 import { Component, For } from 'solid-js';
 
 import styles from './App.module.css';
 
 import { CoffeeCall } from './coffee-call/coffee-call';
+import { Settings } from './settings/settings';
 
 export const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <span class={styles.logo}>☕</span>
-        <CoffeeCall></CoffeeCall>
+        <nav>
+          <NavLink href="/" end={true} >Main</NavLink>
+          <NavLink href="/settings">Settings</NavLink>
+        </nav>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" component={CoffeeCall}/>
+          <Route path="/settings" component={Settings}/>
+        </Routes>
+      </main>
     </div>
   );
 };
--- frontend-web/src/coffee-call/coffee-call.tsx
+++ frontend-web/src/coffee-call/coffee-call.tsx
@@ -1,6 +1,6 @@
 import { Component, For } from 'solid-js';
 
-import styles from './App.module.css';
+import styles from './coffee-call.module.css';
 import { SocketClient } from './socket-client';
 import { Protocol } from './protocol';
 
