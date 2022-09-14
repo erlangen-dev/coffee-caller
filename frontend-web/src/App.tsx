6d9ca9e985e50bdc769caef45234f4141bcd3565
Frank Blendinger <yogan@zogan.de>
2022-09-14 16:25:16 +0200

    Clean up App.tsx

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,5 +1,5 @@
 import { NavLink, Route, Routes } from '@solidjs/router';
-import { Component, For } from 'solid-js';
+import { Component } from 'solid-js';
 
 import styles from './App.module.css';
 
@@ -7,20 +7,18 @@ import { CoffeeCall } from './coffee-call/coffee-call';
 import { Header, Logo } from './scaffolding/header-and-logo';
 import { Settings } from './settings/settings';
 
-export const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <Header>
-        <NavLink href="/" end={true} >Main</NavLink>
-        <NavLink href="/settings">Settings</NavLink>
-      </Header>
-      <Logo />
-      <main>
-        <Routes>
-          <Route path="/" component={CoffeeCall}/>
-          <Route path="/settings" component={Settings}/>
-        </Routes>
-      </main>
-    </div>
-  );
-};
+export const App: Component = () => (
+  <div class={styles.App}>
+    <Header>
+      <NavLink href="/" end={true} >Main</NavLink>
+      <NavLink href="/settings">Settings</NavLink>
+    </Header>
+    <Logo />
+    <main>
+      <Routes>
+        <Route path="/" component={CoffeeCall} />
+        <Route path="/settings" component={Settings} />
+      </Routes>
+    </main>
+  </div>
+);
