563a452f8fd7eee2ad7563f9e3847321dd5643d3
Tilman Adler <tilman-git@t-animal.de>
2022-09-16 15:39:02 +0200

    Add import rewrite paths for absolute imports

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from "path";
 
 export default defineConfig({
   plugins: [solidPlugin()],
@@ -8,5 +9,11 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@features": path.resolve(__dirname, "src/features"),
+    },
   },
 });
