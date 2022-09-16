eec81ac97c37eec0a6c6524b9b31898e21f0d4dd
Tilman Adler <tilman-git@t-animal.de>
2022-09-16 16:09:29 +0200

    move files into folders to restrucure coffee calling

---
+++ frontend-web/src/features/call-for-coffee/index.ts
@@ -0,0 +1,3 @@
+export { aggregatedCoffeeCall, CoffeeCallState, CoffeeCall } from './backend-communication/call-aggregation';
+export { SocketClient } from './backend-communication/socket-client';
+export { Protocol } from './backend-communication/protocol';
