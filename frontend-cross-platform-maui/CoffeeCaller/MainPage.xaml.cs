53ca1581131cf98325315f40d1b5e52397945e40
Frank Blendinger <yogan@zogan.de>
2022-09-19 14:05:57 +0200

    Add Socket.IO client, send messages from buttons

--- frontend-cross-platform-maui/CoffeeCaller/MainPage.xaml.cs
+++ frontend-cross-platform-maui/CoffeeCaller/MainPage.xaml.cs
@@ -2,21 +2,31 @@
 
 public partial class MainPage : ContentPage
 {
+	private const string Name = "Dodd Nedd";
+
+	private readonly SocketClient client;
+
 	public MainPage()
 	{
 		InitializeComponent();
+
+		client = new SocketClient();
+		client.Connect();
 	}
 
 	private void OnJoinClicked(object sender, EventArgs e)
 	{
+		client.Join(Name);
 		SemanticScreenReader.Announce(JoinButton.Text);
 	}
 	private void OnLeaveClicked(object sender, EventArgs e)
 	{
+		client.Leave(Name);
 		SemanticScreenReader.Announce(JoinButton.Text);
 	}
 	private void OnStartClicked(object sender, EventArgs e)
 	{
+		client.Start(Name);
 		SemanticScreenReader.Announce(JoinButton.Text);
 	}
 }
---
+++ frontend-cross-platform-maui/CoffeeCaller/SocketClient.cs
@@ -0,0 +1,57 @@
+﻿using SocketIOClient;
+
+namespace CoffeeCaller;
+
+internal class SocketClient
+{
+	private const string Event = "coffee";
+
+	private readonly SocketIO client;
+
+	public SocketClient()
+	{
+		client = new SocketIO("http://localhost:4200");
+	}
+
+	public async void Connect()
+	{
+
+		client.On(Event, response =>
+		{
+			var message = response.GetValue<Message>();
+		});
+
+		await client.ConnectAsync();
+	}
+
+	public void Join(string name)
+	{
+		client.EmitAsync(Event, new Message(name, MessageType.Join));
+	}
+	public void Leave(string name)
+	{
+		client.EmitAsync(Event, new Message(name, MessageType.Leave));
+	}
+	public void Start(string name)
+	{
+		client.EmitAsync(Event, new Message(name, MessageType.Start));
+	}
+}
+
+#pragma warning disable IDE1006 // Naming Styles
+internal record struct Message(String name, String type);
+#pragma warning restore IDE1006 // Naming Styles
+
+internal class MessageType
+{
+	private readonly string value;
+
+	private MessageType(string type) { value = type; }
+
+	public static implicit operator String(MessageType type) { return type.value; }
+
+	public static MessageType Join => new("join");
+	public static MessageType Leave => new("leave");
+	public static MessageType Start => new("start");
+}
+
