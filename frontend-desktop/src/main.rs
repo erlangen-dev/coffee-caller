065fd44a273d5760e62551eae42b8fde27e85e3e
Richard Baumann <richard.baumann@xitaso.com>
2022-09-20 15:32:25 +0200

    Implement different message types

--- frontend-desktop/src/main.rs
+++ frontend-desktop/src/main.rs
@@ -1,23 +1,28 @@
-use rust_socketio::{ClientBuilder, Payload, Client};
-use serde_json::json;
+use rust_socketio::{Client, ClientBuilder, Payload};
 use serde::Deserialize;
-use std::{rc::Rc, sync::{Mutex, MutexGuard}};
+use serde_json::{json, Value};
 use slint::{Timer, TimerMode};
-
+use std::sync::Arc;
+use std::{
+    rc::Rc,
+    sync::{Mutex, MutexGuard},
+};
 
 slint::slint!(import { MainWindow } from "src/ui/MainWindow.slint";);
 
 #[derive(Deserialize, Debug)]
-struct JoinMessage {
+struct ReceivedMessage {
     name: String,
+    #[serde(rename="type")]
+    message_type: String,
 }
 
-static mutex: Mutex<Option<JoinMessage>> = Mutex::new(None);
+static MUTEX: Mutex<Option<ReceivedMessage>> = Mutex::new(None);
 
 fn receive_string_message(data: &str, _socket: Client) {
-    let join_message: JoinMessage = serde_json::from_str(data).unwrap();
-    let mut guard: MutexGuard<'_, Option<JoinMessage>> = mutex.lock().unwrap();
-    *guard = Some(join_message);
+    let received_message: ReceivedMessage = serde_json::from_str(data).unwrap();
+    let mut guard: MutexGuard<'_, Option<ReceivedMessage>> = MUTEX.lock().unwrap();
+    *guard = Some(received_message);
 }
 
 fn receive_message(payload: Payload, _socket: Client) {
@@ -34,7 +39,7 @@ fn websocket_error(payload: Payload, _: Client) {
     }
 }
 
-fn wsconnect() -> Client{
+fn wsconnect() -> Client {
     // get a socket that is connected to the admin namespace
     let socket = ClientBuilder::new("http://localhost:4200")
         .namespace("/")
@@ -42,12 +47,29 @@ fn wsconnect() -> Client{
         .on("error", websocket_error)
         .connect()
         .expect("Connection failed");
-    return socket
+    return socket;
+}
+
+fn text_for_message(message: &ReceivedMessage) -> &'static str {
+    match message.message_type.as_str() {
+        "join" => return "wants to go for a coffee",
+        "leave" => return "decided against getting a coffee",
+        "start" => return "-> Let's go!",
+        _ => eprintln!(
+            "Incorrect message type received: {:#?}",
+            message.message_type
+        ),
+    }
+    return "";
+}
+
+fn send(websocket: &Client, data: Value, event: &str) {
+    websocket.emit(event, data).expect("Socket down");
 }
 
 fn main() {
     let ui: MainWindow = MainWindow::new();
-    let websocket_client = wsconnect();
+    let websocket_client = Arc::new(wsconnect());
 
     let timer = Timer::default();
 
@@ -55,34 +77,56 @@ fn main() {
         //HistoryItem { text: "wants to get a coffee".into(), user: "Foo".into() },
     ]));
 
-
     let main_window_weak = ui.as_weak();
     let history_model_clone = history_model.clone();
-    timer.start(TimerMode::Repeated, std::time::Duration::from_millis(2), move || {
-        let mut guard: MutexGuard<'_, Option<JoinMessage>> = mutex.lock().unwrap();
-        if let Some(item) = &*guard {
-            let history_item: HistoryItem = HistoryItem { text: "wants to go for a coffee".into(), user: item.name.clone().into() };
-            history_model_clone.push(history_item);
-            *guard = None;
+    timer.start(
+        TimerMode::Repeated,
+        std::time::Duration::from_millis(2),
+        move || {
+            let mut guard: MutexGuard<'_, Option<ReceivedMessage>> = MUTEX.lock().unwrap();
+            if let Some(item) = &*guard {
+                let history_item: HistoryItem = HistoryItem {
+                    text: text_for_message(item).into(),
+                    user: item.name.clone().into(),
+                };
+                history_model_clone.push(history_item);
+                *guard = None;
+            }
+        },
+    );
+    ui.on_call({
+        let websocket_client = websocket_client.clone();
+        move || {
+            let json_payload = json!({"name": "RustCoffee", "type": "join"});
+            send(&websocket_client, json_payload, "coffee");
         }
     });
-    ui.on_call(move || {
-            let json_payload = json!({"name": "RustCoffee"});
-            main_window_weak.unwrap().invoke_add_entry("RustCoffee".into(), "wants to get a coffee".into());
-            websocket_client.emit("coffee", json_payload).expect("Socket down")
+    ui.on_go({
+        let websocket_client = websocket_client.clone();
+        move || {
+            let json_payload = json!({"name": "RustCoffee", "type": "start"});
+            send(&websocket_client, json_payload, "coffee");
         }
-    );
-    
+    });
+    ui.on_leave({
+        let websocket_client = websocket_client.clone();
+        move || {
+            let json_payload = json!({"name": "RustCoffee", "type": "leave"});
+            send(&websocket_client, json_payload, "coffee");
+        }
+    });
     ui.on_add_entry({
         let history_model = history_model.clone();
         move |user, text| {
-            let new_element = HistoryItem { text: text, user: user };
+            let new_element = HistoryItem {
+                text: text,
+                user: user,
+            };
             history_model.push(new_element);
         }
     });
 
     ui.set_history_model(history_model.into());
-
 
     ui.run();
 }
