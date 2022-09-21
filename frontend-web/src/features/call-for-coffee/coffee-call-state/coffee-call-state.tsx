8e3f1c1f692d5c9be0d78b2fe33016774f7e46cd
Tilman Adler <tilman-git@t-animal.de>
2022-09-21 14:08:46 +0200

    Use backend-aggregated coffee calls in web frontend

--- frontend-web/src/features/call-for-coffee/coffee-call-state/coffee-call-state.tsx
+++ frontend-web/src/features/call-for-coffee/coffee-call-state/coffee-call-state.tsx
@@ -1,18 +1,18 @@
 import { Accessor, Component, For, Show, splitProps } from "solid-js";
-import { CoffeeCall, CoffeeCallStatus } from "../backend-communication/call-aggregation";
+import type { CoffeeCall } from "../backend-communication/models";
 
 export const CoffeeCallState: Component<{ coffeeCall: CoffeeCall }> = (props) => {
-  const participantsAsList = () => Array.from(props.coffeeCall.participants.values()).join(',');
+  const participantsAsList = () => props.coffeeCall.participants.join(',');
 
   //TODO: always doing props.coffeeCall is super ugly, but destructuring the props naively doesn't work. can we somehow else do this?
   // We could pass in a Accessor<CoffeeCall> but this is discouraged: https://github.com/solidjs/solid/discussions/749#discussioncomment-1740120
 
   return <>
-    <Show when={props.coffeeCall.status === CoffeeCallStatus.announced}>Coffee call announced!</Show>
-    <Show when={props.coffeeCall.status === CoffeeCallStatus.inProgress}>Coffee call in progress!</Show>
-    <Show when={props.coffeeCall.status === CoffeeCallStatus.canceled}>Coffee call canceled!</Show>
+    <Show when={props.coffeeCall.status === 'announced'}>Coffee call announced!</Show>
+    <Show when={props.coffeeCall.status === 'inProgress'}>Coffee call in progress!</Show>
+    <Show when={props.coffeeCall.status === 'cancelled'}>Coffee call canceled!</Show>
 
-    <Show when={props.coffeeCall.status !== CoffeeCallStatus.inactive && props.coffeeCall.status !== CoffeeCallStatus.canceled}>
+    <Show when={props.coffeeCall.status !== 'inactive' && props.coffeeCall.status !== 'cancelled'}>
       <br />
       On board: {participantsAsList()}
     </Show>
