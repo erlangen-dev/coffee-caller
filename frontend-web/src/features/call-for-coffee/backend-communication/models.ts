650a6d2bbb4b655d2cc4ab2b41a12f4b19e77885
Tilman Adler <tilman-git@t-animal.de>
2022-09-21 14:00:33 +0200

    Copy models over to frontend

---
+++ frontend-web/src/features/call-for-coffee/backend-communication/models.ts
@@ -0,0 +1,20 @@
+const commandTypes = ['join', 'leave', 'start'] as const;
+export type CommandTypes = typeof commandTypes[number];
+
+export interface Command {
+  type: CommandTypes;
+  name: string;
+}
+
+export interface TimedCommand extends Command {
+  broadcastAt: Date;
+}
+
+const coffeeCallStatuses = ['inactive', 'announced', 'inProgress', 'cancelled'] as const;
+export type CoffeeCallStatus = typeof coffeeCallStatuses[number];
+
+export interface CoffeeCall {
+  status: CoffeeCallStatus;
+  participants: string[];
+  messages: TimedCommand[];
+}
