5fa6b37ce0cc742bd9e85ef43d3fdc8633a8e2a0
maxlmo <max.moeller55@gmail.com>
2022-09-22 14:19:47 +0200

    Introduce settings cubit for username

--- frontend-mobile/lib/main.dart
+++ frontend-mobile/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:coffee_caller/environment.dart';
 import 'package:coffee_caller/communication/socket_client.dart';
 import 'package:coffee_caller/screens/home_screen.dart';
 import 'package:coffee_caller/theme.dart';
+import 'package:coffee_caller/widgets/cubit/settings_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -20,6 +22,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Coffee Caller',
       theme: appTheme,
+      builder: (context, child) => BlocProvider(
+        create: (_) => SettingsCubit(),
+        child: child,
+      ),
       home: Provider.value(
         value: socketClient,
         child: const HomeScreen(),
---
+++ frontend-mobile/lib/widgets/cubit/settings_state.freezed.dart
@@ -0,0 +1,152 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'settings_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$SettingsState {
+  SettingsStatus get status => throw _privateConstructorUsedError;
+  String get username => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SettingsStateCopyWith<SettingsState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SettingsStateCopyWith<$Res> {
+  factory $SettingsStateCopyWith(
+          SettingsState value, $Res Function(SettingsState) then) =
+      _$SettingsStateCopyWithImpl<$Res>;
+  $Res call({SettingsStatus status, String username});
+}
+
+/// @nodoc
+class _$SettingsStateCopyWithImpl<$Res>
+    implements $SettingsStateCopyWith<$Res> {
+  _$SettingsStateCopyWithImpl(this._value, this._then);
+
+  final SettingsState _value;
+  // ignore: unused_field
+  final $Res Function(SettingsState) _then;
+
+  @override
+  $Res call({
+    Object? status = freezed,
+    Object? username = freezed,
+  }) {
+    return _then(_value.copyWith(
+      status: status == freezed
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as SettingsStatus,
+      username: username == freezed
+          ? _value.username
+          : username // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_SettingsStateCopyWith<$Res>
+    implements $SettingsStateCopyWith<$Res> {
+  factory _$$_SettingsStateCopyWith(
+          _$_SettingsState value, $Res Function(_$_SettingsState) then) =
+      __$$_SettingsStateCopyWithImpl<$Res>;
+  @override
+  $Res call({SettingsStatus status, String username});
+}
+
+/// @nodoc
+class __$$_SettingsStateCopyWithImpl<$Res>
+    extends _$SettingsStateCopyWithImpl<$Res>
+    implements _$$_SettingsStateCopyWith<$Res> {
+  __$$_SettingsStateCopyWithImpl(
+      _$_SettingsState _value, $Res Function(_$_SettingsState) _then)
+      : super(_value, (v) => _then(v as _$_SettingsState));
+
+  @override
+  _$_SettingsState get _value => super._value as _$_SettingsState;
+
+  @override
+  $Res call({
+    Object? status = freezed,
+    Object? username = freezed,
+  }) {
+    return _then(_$_SettingsState(
+      status == freezed
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as SettingsStatus,
+      username == freezed
+          ? _value.username
+          : username // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SettingsState implements _SettingsState {
+  const _$_SettingsState(
+      [this.status = SettingsStatus.initial, this.username = ""]);
+
+  @override
+  @JsonKey()
+  final SettingsStatus status;
+  @override
+  @JsonKey()
+  final String username;
+
+  @override
+  String toString() {
+    return 'SettingsState(status: $status, username: $username)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_SettingsState &&
+            const DeepCollectionEquality().equals(other.status, status) &&
+            const DeepCollectionEquality().equals(other.username, username));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(status),
+      const DeepCollectionEquality().hash(username));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_SettingsStateCopyWith<_$_SettingsState> get copyWith =>
+      __$$_SettingsStateCopyWithImpl<_$_SettingsState>(this, _$identity);
+}
+
+abstract class _SettingsState implements SettingsState {
+  const factory _SettingsState(
+      [final SettingsStatus status, final String username]) = _$_SettingsState;
+
+  @override
+  SettingsStatus get status;
+  @override
+  String get username;
+  @override
+  @JsonKey(ignore: true)
+  _$$_SettingsStateCopyWith<_$_SettingsState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
