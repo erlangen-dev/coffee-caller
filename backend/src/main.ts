21cd1b17b2488ff4955af394a97ccc5455e0aaff
Tilman Adler <tilman-git@t-animal.de>
2022-09-22 12:54:18 +0200

    Clean up backend code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,34 +1,47 @@
 import { createServer } from "http";
-import { Server } from "socket.io";
-import { Subject, } from "rxjs";
+import { Server, Socket } from "socket.io";
+import { shareReplay, Subject, take, timeout, } from "rxjs";
 
-import { Command } from "./models";
+import { CoffeeCall, Command, TimedCommand } from "./models";
 import { getAllowedCorsOrigins, getServerListeningPort } from "./environment";
 import { convertCommandsToCalls } from "./message-pipeline";
 
+type VoidListener<T> = (event: T) => void;
+type CoffeeCallerEventsMap = { coffee: VoidListener<TimedCommand>, coffeeCalls: VoidListener<CoffeeCall> };
+
 const httpServer = createServer();
-const io = new Server(httpServer, {
+const io = new Server<CoffeeCallerEventsMap>(httpServer, {
     cors: {
         origin: getAllowedCorsOrigins(),
         methods: ["GET", "POST"]
     }
 });
 
+const messageSubject = new Subject<Command>();
+const processedCoffeeCalls = messageSubject.pipe(convertCommandsToCalls);
+
+function broadcastCommand(command: Command) {
+    const timed = { ...command, broadcastAt: new Date() };
+    console.log('→ broadcast command msg:', timed);
+    io.sockets.emit("coffee", timed);
+}
+
+function logCommand(command: Command) {
+    console.log('← received command msg:', command);
+}
+
+function putCommandIntoPipeline(command: Command) {
+    messageSubject.next(command);
+}
+
 io.on("connection", (socket) => {
     console.log(`Got new connection: ${socket.id}`);
-    socket.on("coffee", (commandIn: Command) => {
-        console.log('← received command msg:', commandIn);
-        console.log('→ broadcast command msg:', { ...commandIn, broadcastAt: new Date() });
-        io.sockets.emit("coffee", commandIn);
-        messageSubject.next(commandIn);
-    });
-});
 
-const messageSubject = new Subject<Command>();
-const coffeeCalls = convertCommandsToCalls(messageSubject);
-coffeeCalls.subscribe((coffeeCall) => {
-    console.log('→ broadcast coffeeCall msg:', coffeeCall);
-    io.sockets.emit("coffeeCalls", coffeeCall);
+    socket.on("coffee", logCommand);
+    socket.on("coffee", broadcastCommand)
+    socket.on("coffee", putCommandIntoPipeline);
+
+    processedCoffeeCalls.subscribe((coffeeCall) => socket.emit("coffeeCalls", coffeeCall));
 });
 
 const port = getServerListeningPort();
